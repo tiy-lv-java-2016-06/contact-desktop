f34e2da393e7e9fedfd9e8d4e88cf83ef6c4b7cf
NR44 <darkbrilliance@gmail.com>
2016-06-29 18:07:22 -0700

    Save File works, Load file does not. will continue working.

--- src/contactDesktop/Contact.java
+++ src/contactDesktop/Contact.java
@@ -6,10 +6,26 @@ package contactDesktop;
 public class Contact {
     String nameText, phoneText, emailText;
 
+    public Contact(){
+
+    }
+
     public Contact(String nameText, String phoneText, String emailText) {
         this.nameText = nameText;
         this.phoneText = phoneText;
         this.emailText = emailText;
+    }
+
+    public String getNameText() {
+        return nameText;
+    }
+
+    public String getPhoneText() {
+        return phoneText;
+    }
+
+    public String getEmailText() {
+        return emailText;
     }
 
     @Override
--- src/contactDesktop/Controller.java
+++ src/contactDesktop/Controller.java
@@ -7,11 +7,22 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import jodd.json.JsonParser;
+import jodd.json.JsonSerializer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.URL;
+import java.util.List;
+import java.util.Observable;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 
 public class Controller implements Initializable {
-
+    File file = new File("contacts.json");
+    FileWriter fw = new FileWriter(file);
     ObservableList<Contact> contacts = FXCollections.observableArrayList();
 
     @FXML
@@ -20,7 +31,10 @@ public class Controller implements Initializable {
     @FXML
     TextField nameText, phoneText, emailText;
 
-    public void addContact(){
+    public Controller() throws IOException {
+    }
+
+    public void addContact() throws IOException {
         String name = nameText.getText();
         String phoneNumber = phoneText.getText();
         String email = emailText.getText();
@@ -35,20 +49,44 @@ public class Controller implements Initializable {
         else{
             Contact contact = new Contact(name, phoneNumber, email);
             contacts.add(contact);
+            JsonSerializer serializer = new JsonSerializer();
+            String json = serializer.serialize(contact);
+            fw.write(json);
+            fw.close();
             nameText.setText("");
             phoneText.setText("");
             emailText.setText("");
         }
     }
 
-    public void removeContact(){
-        Contact contact = (Contact)list.getSelectionModel().getSelectedItem();
+    public void removeContact() throws IOException {
+
+       Contact contact = (Contact)list.getSelectionModel().getSelectedItem();
         contacts.remove(contact);
+        JsonSerializer serializer = new JsonSerializer();
+        String json = serializer.serialize(contacts);
+        fw.write(json);
+        fw.close();
+
     }
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        list.setItems(contacts);
+        Scanner fileReader = null;
+        try {
+            fileReader = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        JsonParser fileParser = new JsonParser();
+        //Error is happening below.
+        assert fileReader != null;
+        fileReader.useDelimiter("\\Z");
+        String contents = fileReader.next();
+        //ObservableList<Contact> loadContacts = contacts;
+        //Error is on line 89
+        //contacts = fileParser.parse(contents, ObservableList.class);
+        //list.setItems(contacts);
     }
 }
